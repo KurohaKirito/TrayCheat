e4c39c0abeb9bdab7ceef87c52ce9e7a07c13141
Kuroha <KurohaKirito@gmail.com>
2021-03-16 11:15:33 +0800

    修改回 VS, 并添加密钥认证

--- TrayCheat/WindowsForm.cs
+++ TrayCheat/WindowsForm.cs
@@ -5,17 +5,23 @@ namespace TrayCheat
 {
     public static class WindowsForm
     {
-        [DllImport("user32.dll")]//需要导入该dll
+        // 需要导入该dll
+        [DllImport("user32.dll")]
         private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
         private static extern IntPtr GetForegroundWindow();
 
-        private const int SW_Hide = 0;//隐藏窗口 
-        private const int SW_SHOWRESTORE = 1;//还原窗口, 应用程序在第一次显示窗口的时候应该指定此标志
-        private const int SW_SHOWMINIMIZED = 2;//激活窗口并最小化
-        private const int SW_SHOWMAXIMIZED = 3;//激活窗口并最大化
-        private const int SW_RESTORE = 9;//还原窗口
+        // 隐藏窗口
+        private const int SW_Hide = 0;
+        // 还原窗口, 应用程序在第一次显示窗口的时候应该指定此标志
+        private const int SW_SHOWRESTORE = 1;
+        // 激活窗口并最小化
+        private const int SW_SHOWMINIMIZED = 2;
+        // 激活窗口并最大化
+        private const int SW_SHOWMAXIMIZED = 3;
+        // 还原窗口
+        private const int SW_RESTORE = 9;
 
         public static IntPtr MyGetForegroundWindow()
         {
